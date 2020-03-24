1d8908766937dd4e8830603e5b4ff5d8ddd262b3
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-03-24 17:24:35 -0300

    :sparks: feat: Adicionado rota da listagem de ongs

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,14 @@ const connection = require('./database/connection')
 const routes = express.Router(); 
 // desacoplando o modulo de rotas do express em uma variavel
 
+// rota para listagem das ongs
+routes.get('/ongs', async (request, response) => {
+    const ongs = await connection('ongs').select('*');
+
+    return response.json(ongs);
+})
+
+// rota para cadastro das ongs
 routes.post('/ongs', async (request, response) => {
     const { name, email, whatsapp, city, uf } = request.body;
 
