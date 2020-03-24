3bb0f61f8d635400759e1048857a5527a9b43908
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-03-24 17:41:56 -0300

    :building_construction: chore: Adicionando pasta controllers

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,33 +1,13 @@
 const express = require('express');
-const crypto = require('crypto');
-const connection = require('./database/connection')
+const OngController = require('./controllers/OngController')
 
-const routes = express.Router(); 
 // desacoplando o modulo de rotas do express em uma variavel
+const routes = express.Router(); 
 
 // rota para listagem das ongs
-routes.get('/ongs', async (request, response) => {
-    const ongs = await connection('ongs').select('*');
-
-    return response.json(ongs);
-})
+routes.get('/ongs', OngController.index);
 
 // rota para cadastro das ongs
-routes.post('/ongs', async (request, response) => {
-    const { name, email, whatsapp, city, uf } = request.body;
-
-    const id = crypto.randomBytes(4).toString('HEX');
-
-    await connection('ongs').insert({
-        id,
-        name,
-        email,
-        whatsapp,
-        city,
-        uf,
-    });
-
-    return response.json({ id });
-})
+routes.post('/ongs', OngController.create);
 
 module.exports = routes;
