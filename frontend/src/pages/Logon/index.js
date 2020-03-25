ff332b9dd78ae6d3517130527af3aa79d5576a19
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-03-25 17:42:30 -0300

    :construction: style: Estilizando pagina de logon

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi'
+
 import './styles.css'; // folha de estilo
 
 // imagens
@@ -10,17 +12,20 @@ export default function Logon() {
         <div className="logon-container">
             <section className="form">
                 <img src={logoImg} alt="Logo"/>
-            </section>
-            <form action="">
+
+                <form action="">
                 <h1>Faça seu logon</h1>
 
                 <input placeholder="Sua ID"/>
-                <button type="submit">Entrar</button>
+                <button className="button" type="submit">Entrar</button>
 
                 <a href="/register">
+                    <FiLogIn size={16} color="#e02041" />
                     Não tenho cadastro
                 </a>
             </form>
+            </section>
+            
             <img src={heroesImg} alt="Heroes"/>
         </div>
     );
