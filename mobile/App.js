f3bb74141bfff51f73fead9dcd41335e35901984
Matheusdoe-dev <matheusdoe.dev@gmail.com>
2020-03-26 15:00:44 -0300

    :heavy_plus_sign: feat: Adicionando @react-navigation

--- mobile/App.js
+++ mobile/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Semana Omnistack 11</Text>
+    <View>
+      <Text>Hello World</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
 
-  title: {
-    color: '#f00',
-    fontSize: 36,
-    fontWeight: '700',
-  },
-});
